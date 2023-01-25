f10c7801bf9234c49234266077579bc09d0591ff
NataliaR-BCN <92027716+NataliaR-BCN@users.noreply.github.com>
2023-01-25 21:24:43 +0100

    Revert "Sprint solution v1"

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import Router from './application/Router';
-import { useState } from 'react';
+import './App.css';
+
 
 function App() {
-    
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [userLoggedIn, setUserLoggedIn] = useState('');
-
-  const changeIsAuthenticated = () => setIsAuthenticated(prevIsAuthenticated => !prevIsAuthenticated);
-  const changeUserLoggedIn = (newUserLoggedIn) => setUserLoggedIn(newUserLoggedIn); 
-
   return (
-    
-    <Router 
-            isAuthenticated = {isAuthenticated} 
-            handleIsAuthenticated = {changeIsAuthenticated} 
-            handleUserLoggedIn = {changeUserLoggedIn} 
-            userLoggedIn ={userLoggedIn}         
-    />
+  
+  
+
+  
   );
 }
 
